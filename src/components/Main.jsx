db3a40c8ff8fced0ebb642de7185db7743ba9cb3
Merna <merna.wahdan@gmail.com>
2023-09-12 16:45:16 +0200

    Display number of movies & display movie's image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,15 +15,32 @@ function Main() {
 
 }
 
+    let message;
+    if(moviesToDisplay.length > 0) {
+        message = <h1>Number of movies: {moviesToDisplay.length}</h1>
+
+    } else {
+        message = <h1>Sorry, no movies to display</h1>
+    }
+
 
   return (
     <div className="Main">
-        <h1>Number of movies: {moviesToDisplay.length}</h1>
+            {message}
       {moviesToDisplay.map((e) => {
         return (
           <section key={e.id} className="card">
             <h2>{e.title}</h2>
+
+            { e.imgURL 
+            ? <img src={e.imgURL}/>
+            :  <img src="https://dummyimage.com/182x268/ffffff/000000" /> 
+            }
+
             <h3>Rating: {e.rating}</h3>
+            {e.rating >= 8 && <p>RECOMMENDED</p>}  
+            {/* if the 1st part is true, && > apply this/render this */}
+
             <button onClick={() => {deleteMovie(e.id)}}>Delete</button>
           </section>
         );
